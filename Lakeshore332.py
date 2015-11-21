22bdd431c049cfc598fe3599733c5e5111c0333a
PeterMintun <peter.mintun@gmail.com>
2015-11-21 00:09:31 -0600

    not sure
    did a lot of stuff to try and figure out what's going on. but the basic
    lantz query function they suggest in the driver tutorial makes it
    timeout and not communicate with the instrument.
    
    still want to improve the design to follow proper coding practice better
    but not playing nice with whatever the @Feats() and __init__ functions
    do. basically confused by lanz

--- Lakeshore332.py
+++ Lakeshore332.py
@@ -12,65 +12,93 @@ class Lakeshore332(MessageBasedDriver):
     """
     Lakeshore 332 Temperature controlller.
 
-    This class implements a set of basic controls for the Lakeshore 332 series
-    temperature controller.
+    This class, based off of the Lantz MessageBasedDriver class, implements a
+    set of basic controls for the Lakeshore 332 series temperature controller.
+    It essentially a port of a nice driver written for QtLab by Reinier Heeres.
 
     Full documentation of the device is available at:
     http://www.lakeshore.com/ObsoleteAndResearchDocs/332_Manual.pdf
     """
 
-    def __init__(self, name, address, reset=False):
-        MessageBasedDriver.__init__(self,name)
-        self._address = address
-        self._verbose = True
+    # These defaults assume that you have set the IEEE Term setting to: Lf Cr
+    DEFAULTS = {'COMMON': {'write_termination': '\n',
+                          'read_termination': '\n'}}
 
-        self.channel_list = ['A','B']
-        self.heater_range_vals = {'off': 0, 'low': 1, 'medium': 2, 'high': 3}
-        self.heater_status_vals = {'no error':0, 'open load':1, 'short':2}
-        self.controller_modes = {'local': 0, 'remote':1, 'remote, local lockout': 2}
+    channel_list = ['A','B']
+    heater_range_vals = {'off': 0, 'low': 1, 'medium': 2, 'high': 3}
+    heater_status_vals = {'no error':0, 'open load':1, 'short':2}
+    controller_modes = {'local': 0, 'remote':1, 'remote, local lockout': 2}
 
-        if reset:
-            self.reset()
-        else:
-            self.get_all()
+    # def __init__(self, GPIB_name, GPIB_addr, reset=False):
+    #    res_name = GPIB_name + '::' + GPIB_addr + '::INSTR'
+    #    super().__init__(res_name, name='Lakeshore332')
+    #    self._address = res_name
+    _verbose = True
+    #    self._idn = None
+    #
+    #    print(res_name)
+    #
+    #    print(self.channel_list)
+    #    print(self.heater_range_vals)
+    #    print(self.heater_status_vals)
+    #    print(self.controller_modes)
 
-     DEFAULTS = {'COMMON': {'write_termination': '\n',
-                           'read_termination': '\n'}}
 
+
+    # def query(self, command, *, send_args=(None, None), recv_args=(None, None)):
+    #      answer = super().query(command, send_args=send_args, recv_args=recv_args)
+    #      if answer == 'ERROR':
+    #          raise InstrumentError
+    #          return answer
+
+    #def initialize(self, reset=False):
+    #    """
+    #    Initialization code for Lakeshore 332 should go in this function, since
+    #    if you define functions as @Feats() they cannot be executed in the
+    #    __init__ function.
+    #    """
+        #if (self._verbose):
+            #print('initializing Lakeshore 332...')
+
+        #if reset:
+            #if self._verbose:
+                #print('Resetting...')
+            #self.reset
+        #else:
+            #self.idn()
+            #self.mode()
+
+    # def get_all(self):
+    #    """
+    #    Gets the instrument identification and mode.
+    #    """
+    #    self.idn()
+    #    self.mode()
 
     @Action()
     def reset(self):
-        """
-        Resets the Lakeshore 332 temperature controller.
-        """
-        self.write('*RST')
-
-    def get_all(self):
-        """
-        Gets the instrument identification and mode.
-        """
-        print(self.idn())
-        print(self.mode())
-        return None
+         """
+         Resets the Lakeshore 332 temperature controller.
+         """
+         self.write('*RST')
 
     @Feat()
-    def get_idn(self):
+    def idn(self):
         """
         Returns the instrument identification.
         """
         return self.query('*IDN?')
 
     @Feat()
-    def get_kelvin(self, channel):
-        """
-        Returns temperature reading from specified channel in Kelvin.
-        """
-        if channel in channel_list:
-            msg_str = 'KRDG?' + channel
-            return self.query(msg_str.format(channel))
-        else:
-            print('Error: invalid channel')
-            return None
+    def kelvin(self, channel):
+         """
+         Returns temperature reading from specified channel in Kelvin.
+         """
+         if channel in channel_list:
+             return self.query('KRDG?{}'.format(channel))
+         else:
+             print('Error: invalid channel')
+             return None
 
     @Feat()
     def get_sensor(self, channel):
@@ -84,15 +112,16 @@ class Lakeshore332(MessageBasedDriver):
             print('Error: invalid channel')
             return None
 
-    @Feat(values=self.heater_status_vals)
+    #@Feat(values = self.heater_status_vals)
+    @Feat()
     def get_heater_status(self):
         """
-        Returns the header status.
+        Returns the heater status.
         """
         ans = self.query('HTRST?')
         if self._verbose:
             if ans == '0':
-                print('Heater status: no error)
+                print('Heater status: no error')
             elif ans == '1':
                 print('Heater status: open load')
             elif ans == '2':
@@ -101,8 +130,9 @@ class Lakeshore332(MessageBasedDriver):
                 print('Error: Heater status invalid')
         return ans
 
-    @Feat(values=self.heater_range_vals)
-    def get_heater_range(self):
+    #@Feat(values=self.heater_range_vals)
+    @Feat()
+    def heater_range(self):
         """
         Queries the instrument, prints a message describing the current heater
         range setting, then returns the heater range value.
@@ -131,15 +161,16 @@ class Lakeshore332(MessageBasedDriver):
         return self.write('RANGE {}'.format(heater_setting))
 
 
-    @Feat(values=controller_modes)
-    def get_mode(self):
+    #@Feat(values=self.controller_modes)
+    @Feat()
+    def mode(self):
         """
         Reads the mode setting of the controller.
         """
         return self.query('MODE?')
 
-    @get_mode.setter
-    def get_mode(self, mode):
+    @mode.setter
+    def mode(self, mode):
         """
         Sets controller mode, valid mode inputs are:
         local
@@ -163,7 +194,7 @@ class Lakeshore332(MessageBasedDriver):
         fields = [float(f) for f in fields]
         return fields
 
-    @get_pid.setter
+    @pid.setter
     def pid(self, val, channel):
         """
         Need to actually implement this!
@@ -222,5 +253,7 @@ class Lakeshore332(MessageBasedDriver):
 
 
 if __name__ == '__main__':
-    with Lakeshore332('GPIB::GPIB0::16::INSTR') as inst:
-        print('The identification of this instrument is : ' + inst.idn)
+    with Lakeshore332('GPIB0::16::INSTR') as inst:
+        print('The instrument identification is ' + inst.idn)
+        #print('The current temperature is '+ inst.kelvin(channel='A'))
+        #inst.initialize(reset=False)
