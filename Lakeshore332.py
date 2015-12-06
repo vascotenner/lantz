19b348ce68b3950a18f99b79f93e406dfa9406c6
Peter Mintun <peter.mintun@gmail.com>
2015-12-06 15:51:02 -0600

    finally getting comm fixed
    looks like the read termination I was using was wrong. things should be
    working now.

--- Lakeshore332.py
+++ Lakeshore332.py
@@ -5,7 +5,7 @@
 
 # Some sort of license information goes here.
 
-from lantz import Feat, Action
+from lantz import Feat, Action, DictFeat
 from lantz.messagebased import MessageBasedDriver
 
 class Lakeshore332(MessageBasedDriver):
@@ -22,12 +22,15 @@ class Lakeshore332(MessageBasedDriver):
 
     # These defaults assume that you have set the IEEE Term setting to: Lf Cr
     DEFAULTS = {'COMMON': {'write_termination': '\n',
-                          'read_termination': '\n'}}
+                          'read_termination': ''}}
+
+    GPIB_name = None
+    GPIB_address = -1
 
     channel_list = ['A','B']
     heater_range_vals = {'off': 0, 'low': 1, 'medium': 2, 'high': 3}
     heater_status_vals = {'no error':0, 'open load':1, 'short':2}
-    controller_modes = {'local': 0, 'remote':1, 'remote, local lockout': 2}
+    controller_modes = {'local': 0, 'remote': 1, 'remote, local lockout': 2}
 
     # def __init__(self, GPIB_name, GPIB_addr, reset=False):
     #    res_name = GPIB_name + '::' + GPIB_addr + '::INSTR'
@@ -74,186 +77,198 @@ class Lakeshore332(MessageBasedDriver):
     #    """
     #    self.idn()
     #    self.mode()
+    @Feat()
+    def idn(self):
+        """
+        Returns the instrument identification.
+        """
+        print('getting IDN')
+        return self.query('*IDN?')
 
     @Action()
     def reset(self):
          """
          Resets the Lakeshore 332 temperature controller.
          """
+         print("resetting")
          self.write('*RST')
+         print("reset")
 
-    @Feat()
-    def idn(self):
-        """
-        Returns the instrument identification.
-        """
-        return self.query('*IDN?')
-
-    @Feat()
-    def kelvin(self, channel):
-         """
-         Returns temperature reading from specified channel in Kelvin.
-         """
-         if channel in channel_list:
-             return self.query('KRDG?{}'.format(channel))
-         else:
-             print('Error: invalid channel')
-             return None
-
-    @Feat()
-    def get_sensor(self, channel):
-        """
-        Returns sensor reading from specified channel.
-        """
-        if channel in channel_list:
-            msg_str = 'SRDG?' + channel
-            return self.query(msg_str.format(channel))
-        else:
-            print('Error: invalid channel')
-            return None
-
-    #@Feat(values = self.heater_status_vals)
-    @Feat()
-    def get_heater_status(self):
-        """
-        Returns the heater status.
-        """
-        ans = self.query('HTRST?')
-        if self._verbose:
-            if ans == '0':
-                print('Heater status: no error')
-            elif ans == '1':
-                print('Heater status: open load')
-            elif ans == '2':
-                print('Heater status: short')
-            else:
-                print('Error: Heater status invalid')
-        return ans
-
-    #@Feat(values=self.heater_range_vals)
-    @Feat()
-    def heater_range(self):
-        """
-        Queries the instrument, prints a message describing the current heater
-        range setting, then returns the heater range value.
-        """
-        ans = self.query('RANGE?')
-        if self._verbose:
-            if ans == '0':
-                print('Heater range: off')
-            elif ans == '1':
-                print('Heater range: low')
-            elif ans == '2':
-                print('Heater range: medium')
-            elif ans == '3':
-                print('Heater range: high')
-            else:
-                print('Error: Heater not responding correctly')
-        return ans
-
-    @heater_range.setter
-    def heater_range(self, heater_setting):
-        """
-        Sets heater range to  heater_setting.
-
-        heater_setting must be an integer between 0 and 3 inclusive.
-        """
-        return self.write('RANGE {}'.format(heater_setting))
-
-
-    #@Feat(values=self.controller_modes)
-    @Feat()
+    # @Feat()
+    # def kelvin(self, channel):
+    #      """
+    #      Returns temperature reading from specified channel in Kelvin.
+    #      """
+    #      if channel in channel_list:
+    #          return self.query('KRDG?{}'.format(channel))
+    #      else:
+    #          print('Error: invalid channel')
+    #          return None
+    #
+    # @Feat()
+    # def get_sensor(self, channel):
+    #     """
+    #     Returns sensor reading from specified channel.
+    #     """
+    #     if channel in channel_list:
+    #         msg_str = 'SRDG?' + channel
+    #         return self.query(msg_str.format(channel))
+    #     else:
+    #         print('Error: invalid channel')
+    #         return None
+    #
+    # #@Feat(values = self.heater_status_vals)
+    # @Feat()
+    # def get_heater_status(self):
+    #     """
+    #     Returns the heater status.
+    #     """
+    #     ans = self.query('HTRST?')
+    #     if self._verbose:
+    #         if ans == '0':
+    #             print('Heater status: no error')
+    #         elif ans == '1':
+    #             print('Heater status: open load')
+    #         elif ans == '2':
+    #             print('Heater status: short')
+    #         else:
+    #             print('Error: Heater status invalid')
+    #     return ans
+    #
+    # #@Feat(values=self.heater_range_vals)
+    # @Feat()
+    # def heater_range(self):
+    #     """
+    #     Queries the instrument, prints a message describing the current heater
+    #     range setting, then returns the heater range value.
+    #     """
+    #     ans = self.query('RANGE?')
+    #     if self._verbose:
+    #         if ans == '0':
+    #             print('Heater range: off')
+    #         elif ans == '1':
+    #             print('Heater range: low')
+    #         elif ans == '2':
+    #             print('Heater range: medium')
+    #         elif ans == '3':
+    #             print('Heater range: high')
+    #         else:
+    #             print('Error: Heater not responding correctly')
+    #     return ans
+    #
+    # @heater_range.setter
+    # def heater_range(self, heater_setting):
+    #     """
+    #     Sets heater range to  heater_setting.
+    #
+    #     heater_setting must be an integer between 0 and 3 inclusive.
+    #     """
+    #     return self.write('RANGE {}'.format(heater_setting))
+    #
+    #
+    @Feat(values=controller_modes)
     def mode(self):
         """
         Reads the mode setting of the controller.
         """
-        return self.query('MODE?')
+        return int(self.query('MODE?'))
 
     @mode.setter
     def mode(self, mode):
         """
         Sets controller mode, valid mode inputs are:
-        local
-        remote
-        remote, local lockout
+        local (0)
+        remote (1)
+        remote, local lockout (2)
         """
-        return self.query('MODE {}'.format(value))
-
-    def local(self):
-        self.set_mode('local')
-
-    def remote(self):
-        self.set_mode('remote')
-
-    @Feat()
-    def pid(self, channel):
-        ans = self.query('PID? {}'.format(channel))
-        fields = ans.split(',')
-        if len(fields) != 3:
-            return None
-        fields = [float(f) for f in fields]
-        return fields
-
-    @pid.setter
-    def pid(self, val, channel):
-        """
-        Need to actually implement this!
-        """
-        print('This feature is actually unimplemented! Sorry!')
-
-    @Feat()
-    def setpoint(self, channel):
-        """
-        Return the feedback controller's setpoint.
-        """
-        if channel in self.channel_list:
-            return self.query('SETP?{}'.format(channel))
-        else:
-            return None
-
-    @setpoint.setter
-    def setpoint(self, channel, value):
-        """
-        Sets the setpoint of channel channel to value value
-        """
-        #TODO: actually implement this!
-        print('Not yet implemented!')
-
-    @Feat()
-    def mout(self):
-        """
-        """
-        #TODO: actually implement this!
-        print('Not yet implemented!')
-
-    @mout.setter
-    def mout(self, value):
-        """
-        """
-        #TODO: actually implement this!
-        print('Not yet implemented!')
-
-    @Feat()
-    def cmd_mode(self):
-        """
-        """
-        #TODO: actually implement this!
-        print('Not yet implemented!')
-
-    @cmd_mode.setter
-    def cmd_mode(self, value):
-        """
-        """
-        #TODO: implement this!
-        print('Not yet implemented!')
-
-
-
-
-
+        return self.query('MODE{}'.format(mode))
+    #
+    # def local(self):
+    #     self.set_mode('local')
+    #
+    # def remote(self):
+    #     self.set_mode('remote')
+    #
+    # @Feat()
+    # def pid(self, channel):
+    #     ans = self.query('PID? {}'.format(channel))
+    #     fields = ans.split(',')
+    #     if len(fields) != 3:
+    #         return None
+    #     fields = [float(f) for f in fields]
+    #     return fields
+    #
+    # @pid.setter
+    # def pid(self, val, channel):
+    #     """
+    #     Need to actually implement this!
+    #     """
+    #     print('This feature is actually unimplemented! Sorry!')
+    #
+    # @Feat()
+    # def setpoint(self, channel):
+    #     """
+    #     Return the feedback controller's setpoint.
+    #     """
+    #     if channel in self.channel_list:
+    #         return self.query('SETP?{}'.format(channel))
+    #     else:
+    #         return None
+    #
+    # @setpoint.setter
+    # def setpoint(self, channel, value):
+    #     """
+    #     Sets the setpoint of channel channel to value value
+    #     """
+    #     #TODO: actually implement this!
+    #     print('Not yet implemented!')
+    #
+    # @Feat()
+    # def mout(self):
+    #     """
+    #     """
+    #     #TODO: actually implement this!
+    #     print('Not yet implemented!')
+    #
+    # @mout.setter
+    # def mout(self, value):
+    #     """
+    #     """
+    #     #TODO: actually implement this!
+    #     print('Not yet implemented!')
+    #
+    # @Feat()
+    # def cmd_mode(self):
+    #     """
+    #     """
+    #     #TODO: actually implement this!
+    #     print('Not yet implemented!')
+    #
+    # @cmd_mode.setter
+    # def cmd_mode(self, value):
+    #     """
+    #     """
+    #     #TODO: implement this!
+    #     print('Not yet implemented!')
+    #
+    #
+    #
+    #
+    #
 
 if __name__ == '__main__':
     with Lakeshore332('GPIB0::16::INSTR') as inst:
+        print('Getting instrument identification...')
+        #inst.query('*IDN?')
         print('The instrument identification is ' + inst.idn)
+        print('reset?')
+        inst.reset
+        print('reset.')
+        print('The current mode is ' + inst.mode + '.')
+        print(inst.controller_modes)
+        inst.mode = 'remote, local lockout'
+        print('Now the mode is ' + inst.mode + '.')
+        inst.mode = 'remote'
+        print('Now the mode is ' + inst.mode + '.')
         #print('The current temperature is '+ inst.kelvin(channel='A'))
         #inst.initialize(reset=False)
