5d3cb5ed71b53fef0df67645916077433dd26824
Peter Mintun <peter.mintun@gmail.com>
2015-12-08 14:48:09 -0600

    adding more functionality to LIA
    added more function to lockin driver, also testing slack integration

--- SignalRecovery7265.py
+++ SignalRecovery7265.py
@@ -18,24 +18,31 @@ class SignalRecovery7265(MessageBasedDriver):
     follows:
 
     ADDRESS = N
-    TERMINATOR = [CR],[LF]
+    TERMINATOR = [EOI]
     TECH ECHO = DISABLED
 
     SDC=ADF 1 ENABLED
     """
 
-    DEFAULTS = {'COMMON': {'write_termination': '\n',
-                           'read_termination': '\n'}}
+    DEFAULTS = {'COMMON': {'write_termination': '',
+                           'read_termination': ''}}
 
     sensitivities = {}
 
-    time_constants = {0: '10e-6', 1: '20e-6', 2: '40e-6', 3: '80e-6',
-                      4: '160e-6', 5: '320e-6', 6: '640e-6', 7: '5e-3',
-                      8: '10e-3', 9: '20e-3', 10: '50e-3', 11: '100e-3',
-                      12: '200e-3', 13: '500e-3', 14: '1', 15: '2', 16: '5',
-                      17: '10', 18: '20', 19: '50', 20: '100', 21: '200',
-                      22: '500', 23: '1e3', 24: '2e3', 25: '5e3', 26: '10e3',
-                      27: '20e3', 28: '50e3', 29: '100'}
+    time_constants = {0: 10e-6, 1: 20e-6, 2: 40e-6, 3: 80e-6, 4: 160e-6,
+                      5: 320e-6, 6: 640e-6, 7: 5e-3, 8: 10e-3, 9: 20e-3,
+                      10: 50e-3, 11: 100e-3, 12: 0.2, 13: 0.5, 14: 1, 15: 2,
+                      16: 5, 17: 10, 18: 20, 19: 50, 20: 100, 21: 200, 22: 500,
+                      23: 1e3, 24: 2e3, 25: 5e3, 26: 10e3, 27: 20e3, 28: 50e3,
+                      29: 100}
+
+    # time_constants = {10e-6: 0, 20e-6: 1, 40e-6: 2, 80e-6: 3,
+    #                    160e-6: 4, 320e-6: 5, 640e-6: 6, 5e-3: 7,
+    #                    10e-3: 8, 20e-3: 9, 50e-3: 10, 100e-3: 11,
+    #                    200e-3: 12, 500e-3: 13, 1: 14, 2: 15, 5: 16,
+    #                    10: 17, 20: 18, 50: 19, 100: 20, 200: 21,
+    #                    500: 22, 1e3: 23, 2e3: 24, 5e3: 25, 10e3: 26,
+    #                    20e3: 27, 50e3: 28, 100: 29}
 
     int_ext_refs = {'int': 0, 'rear ext': 1, 'ext': 2}
 
@@ -51,82 +58,104 @@ class SignalRecovery7265(MessageBasedDriver):
         """
         Read x value from lockin.
         """
-        return float(self.query('X.'))
+        read = self.query('X.')
+        return float(read.replace('\x00', ''))
 
     @Feat()
     def y(self):
         """
         Read y value from lockin.
         """
-        return float(self.query('Y.'))
+        read = self.query('Y.')
+        return float(read.replace('\x00', ''))
 
     @Feat()
     def xy(self):
         """
         Read x and y values from lockin simultaneously.
         """
-        return self.query('XY.')
+        read = self.query('XY.')
+        return [float(x) for x in read.replace('\x00', '').split(',')]
 
     @Feat()
     def magnitude(self):
         """
         Read signal magnitude from lockin.
         """
-        return float(self.query('MAG.'))
+        read = self.query('MAG.')
+        return float(read.replace('\x00', ''))
 
     @Feat()
     def phase(self):
         """
         Read signal phase from lockin.
         """
-        return float(self.query('PHA.'))
+        read = self.query('PHA.')
+        return float(read.replace('\x00', ''))
 
     @Feat()
     def mag_phase(self):
         """
         Read signal magnitude and phase from lockin.
         """
-        return self.query('MP.')
+        read = self.query('MP.')
+        return [float(x) for x in read.replace('\x00', '').split(',')]
 
     # TODO: time constant
-    @Feat()
-    def time_constant(self):
+    @Feat(limits=(0, 29, 1))
+    def time_constant_integer(self):
         """
-        Read current lockin time constant
+        Read current lockin time constant mode setting
         """
-        print('not implemented yet')
-        return 0
+        return int(self.query('TC'))
 
-    @time_constant.setter
-    def time_constant(self, value):
+    @time_constant_integer.setter
+    def time_constant_integer(self, integer):
         """
         Set lockin time constant.
         """
-        print('not implemented yet')
-        return 0
+        return self.write('TC{}'.format(integer))
 
-    # TODO: frequency
+    @Feat(values=time_constants)
+    def time_constant(self):
+        """
+        Returns current time constant setting (in seconds).
+        """
+        return float(self.query('TC.'))
+
+    @time_constant.setter
+    def time_constant(self, time_const):
+        """
+        Sets the current time constant setting (in seconds).
+        """
+        return self.write('TC.{}'.format(time_const))
+
     @Feat()
     def frequency(self):
         """
         Read current signal frequency.
         """
-        return float(self.query('FRQ.'))
+        read = self.query('FRQ.')
+        return float(read.replace('\x00', ''))
 
-    @frequency.setter
-    def frequency(self, freq):
+    @Feat(limits=(0, 250e3))
+    def oscillator_freq(self):
         """
-        Returns the frequency of the reference signal (either internal or
-        externally sourced) in Hz.
+        Read internal oscillator frequency.
         """
-        print('not implemented yet')
-        return 0
+        return float(self.query('OF.'))
+
+    @oscillator_freq.setter
+    def oscillator_freq(self, frequency):
+        """
+        Set internal oscillator frequency.
+        """
+        return self.write('OF.{}'.format(frequency))
 
     # TODO: gain
 
     # TODO: sensitivity
 
-    # TODO: reference internal/external
     @Feat(values=int_ext_refs)
     def int_ext_ref(self):
         """
@@ -139,9 +168,7 @@ class SignalRecovery7265(MessageBasedDriver):
         """
         Set lockin to be internal or external reference
         """
-        # TODO: fix this!
-        print('Warning: fails intermittently, need to fix')
-        return self.query('IE {}'.format(value))
+        return self.write('IE {}'.format(value))
 
 
 if __name__ == '__main__':
@@ -155,12 +182,29 @@ if __name__ == '__main__':
         print('Signal phase is ' + str(inst.phase) + 'degrees')
 
         print('Testing full quadrature readings')
-        print('XY measurement: ' + inst.xy)
-        print('magnitude and phase measurement: ' + inst.mag_phase)
+        print('XY measurement: ' + str(inst.xy))
+        print('magnitude and phase measurement: ' + str(inst.mag_phase))
 
+        print('What\'s the frequency, Kenneth?')
+        print('Reference frequency: ' + str(inst.frequency) + 'Hz')
+        inst.oscillator_freq = 137.0
+        print('Reference frequency: ' + str(inst.frequency) + 'Hz')
+        inst.oscillator_freq = 17
+        print('Reference frequency: ' + str(inst.frequency) + 'Hz')
+        #
         print('Internal External Reference check')
         print('Internal/ext reference: ' + inst.int_ext_ref)
-        #inst.int_ext_ref = 'ext'
+        inst.int_ext_ref = 'ext'
         print('Internal/ext reference: ' + inst.int_ext_ref)
-        #inst.int_ext_ref = 'int'
+        inst.int_ext_ref = 'int'
         print('Internal/ext reference: ' + inst.int_ext_ref)
+
+        print('Time constant check')
+        print('Int TC: ' + str(inst.time_constant_integer))
+        print('TC (sec): ' + str(inst.time_constant))
+        inst.time_constant_integer = 15
+        print('Int TC: ' + str(inst.time_constant_integer))
+        print('TC (sec): ' + str(inst.time_constant))
+        inst.time_constant = 100e-3
+        print('Int TC: ' + str(inst.time_constant_integer))
+        print('TC (sec): ' + str(inst.time_constant))
