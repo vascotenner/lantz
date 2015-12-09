58c4740359e600474836763b283658b0126fec68
Peter Mintun <peter.mintun@gmail.com>
2015-12-09 15:45:12 -0600

    added sensitivities to SR7265, SP2150 is still derp
    this driver should be done...still missing comments and some logging
    functionality, but it should be able to be useful for most stuff you
    might want to do experimentally.
    
    SP2150 I can communicate w/ over USB, but there are some weird things
    going on where it drops commands or something. still debugging this.

--- SP2150i.py
+++ SP2150i.py
@@ -1,43 +1,60 @@
 from lantz import Feat, DictFeat, Action
 from lantz.messagebased import MessageBasedDriver
 
+from time import sleep
+
 
 class SP2150i(MessageBasedDriver):
+    """
 
+    """
+    MANUFACTURER_ID = '0x0647'
+    MODEL_CODE = '0x0100'
+
+    DEFAULTS = {'COMMON': {'write_termination': '\r',
+                           'read_termination': ''}}
 
     max_speed = 100
     wavelength_min = 380
     wavelength_max = 520
 
-    @Feat(limits=(0,max_speed))
+    @Feat(limits=(0, max_speed))
     def scan_speed(self):
         """
         Get scan rate in nm/min.
         """
-        return float(self.query('?NM/MIN'))
+        return self.query('?NM/MIN')
 
     @scan_speed.setter
     def scan_speed(self, speed):
         """
         Sets current scan speed in nm/min.
         """
-        return self.query('{} NM/MIN'.format(speed))
+        read = self.query('{} NM/MIN'.format(speed))
+        read2 = read.replace('nm/min  ok', '')
+        print('nm/min read:' + read2)
+        sleep(1)
+        return read
 
     @Feat(limits=(wavelength_min, wavelength_max))
     def nm(self):
         """
-        Returns  wavelength to nm.
         """
-        return float(self.query('?NM'))
+        print('Sending ?NM...')
+        read = self.query('?NM')
+        read = read.replace('nm  ok', '')
+        read = read.replace('1` ', '')
+        return float(read)
 
     @nm.setter
     def nm(self, wavelength):
         """
-        Sets output to specified wavelength, traveling at the current scan rate.
+        Sets output to specified wavelength, traveling at the current scan
+        rate.
         """
         return self.query('{} NM'.format(wavelength))
 
-    @Feat(limits=(1,2,1))
+    @Feat(limits=(1, 2, 1))
     def grating(self):
         """
         Returns the current grating position
@@ -53,7 +70,7 @@ class SP2150i(MessageBasedDriver):
         self.query('{} GRATING'.format(grating_num))
         sleep(20)
 
-    @Feat(limits=(1,3,1))
+    @Feat(limits=(1, 3, 1))
     def turret(self):
         """
         Returns the selected turret number.
@@ -83,10 +100,9 @@ class SP2150i(MessageBasedDriver):
         return self.query('?GRATINGS')
 
 
-
-
-
-
-    if __name__ == '__main__':
-        with SP2150i('USB derp derp derp') as inst:
-            print('The instrument identification is ' + inst.idn)
+if __name__ == '__main__':
+    with SP2150i('USB0::0x0647::0x0100::NI-VISA-120001::RAW') as inst:
+        print('Testing 1, 2, 3')
+        print('Wavelength: {}'.format(inst.nm))
+        print('Scan rate: {}'.format(inst.scan_speed))
+        # inst.nm = 400.0
--- SignalRecovery7265.py
+++ SignalRecovery7265.py
@@ -1,6 +1,8 @@
 from lantz import Feat, DictFeat, Action
 from lantz.messagebased import MessageBasedDriver
 
+from collections import OrderedDict
+
 from time import sleep
 
 
@@ -27,24 +29,87 @@ class SignalRecovery7265(MessageBasedDriver):
     DEFAULTS = {'COMMON': {'write_termination': '',
                            'read_termination': ''}}
 
-    sensitivities = {}
+    INT_EXT_REF = OrderedDict([
+                   ('int', 0),
+                   ('rear ext', 1),
+                   ('ext', 2)
+                   ])
 
-    time_constants = {0: 10e-6, 1: 20e-6, 2: 40e-6, 3: 80e-6, 4: 160e-6,
-                      5: 320e-6, 6: 640e-6, 7: 5e-3, 8: 10e-3, 9: 20e-3,
-                      10: 50e-3, 11: 100e-3, 12: 0.2, 13: 0.5, 14: 1, 15: 2,
-                      16: 5, 17: 10, 18: 20, 19: 50, 20: 100, 21: 200, 22: 500,
-                      23: 1e3, 24: 2e3, 25: 5e3, 26: 10e3, 27: 20e3, 28: 50e3,
-                      29: 100}
+    TIME_CONSTANTS = OrderedDict([
+                     (0, 10e-6),
+                     (1, 20e-6),
+                     (2, 40e-6),
+                     (3, 80e-6),
+                     (4, 160e-6),
+                     (5, 320e-6),
+                     (6, 640e-6),
+                     (7, 5e-3),
+                     (8, 10e-3),
+                     (9, 20e-3),
+                     (10, 50e-3),
+                     (11, 100e-3),
+                     (12, 0.2),
+                     (13, 0.5),
+                     (14, 1),
+                     (15, 2),
+                     (16, 5),
+                     (17, 10),
+                     (18, 20),
+                     (19, 50),
+                     (20, 100),
+                     (21, 200),
+                     (22, 500),
+                     (23, 1e3),
+                     (24, 2e3),
+                     (25, 5e3),
+                     (26, 10e3),
+                     (27, 20e3),
+                     (28, 50e3),
+                     (29, 100)
+                     ])
 
-    # time_constants = {10e-6: 0, 20e-6: 1, 40e-6: 2, 80e-6: 3,
-    #                    160e-6: 4, 320e-6: 5, 640e-6: 6, 5e-3: 7,
-    #                    10e-3: 8, 20e-3: 9, 50e-3: 10, 100e-3: 11,
-    #                    200e-3: 12, 500e-3: 13, 1: 14, 2: 15, 5: 16,
-    #                    10: 17, 20: 18, 50: 19, 100: 20, 200: 21,
-    #                    500: 22, 1e3: 23, 2e3: 24, 5e3: 25, 10e3: 26,
-    #                    20e3: 27, 50e3: 28, 100: 29}
+    AC_GAINS = OrderedDict([
+               ('0dB', 0),
+               ('10dB', 1),
+               ('20dB', 2),
+               ('30dB', 3),
+               ('40dB', 4),
+               ('50dB', 5),
+               ('60dB', 6),
+               ('70dB', 7),
+               ('80dB', 8),
+               ('90dB', 9),
+               ])
 
-    int_ext_refs = {'int': 0, 'rear ext': 1, 'ext': 2}
+    SENSITIVITIES = OrderedDict([
+                    ('2e-9V', 1),
+                    ('5e-9V', 2),
+                    ('1e-8V', 3),
+                    ('2e-8V', 4),
+                    ('5e-8V', 5),
+                    ('1e-7V', 6),
+                    ('2e-7V', 7),
+                    ('5e-7V', 8),
+                    ('1e-6V', 9),
+                    ('2e-6V', 10),
+                    ('5e-6V', 11),
+                    ('1e-5V', 12),
+                    ('2e-5V', 13),
+                    ('5e-5V', 14),
+                    ('1e-4V', 15),
+                    ('2e-4V', 16),
+                    ('5e-4V', 17),
+                    ('1e-3V', 18),
+                    ('2e-3V', 19),
+                    ('5e-3V', 20),
+                    ('1e-2V', 21),
+                    ('2e-2V', 22),
+                    ('5e-2V', 23),
+                    ('1e-1V', 24),
+                    ('2e-1V', 25),
+                    ('5e-1V', 26),
+                    ('1V', 27),
+                    ])
 
     @Feat()
     def idn(self):
@@ -101,7 +166,6 @@ class SignalRecovery7265(MessageBasedDriver):
         read = self.query('MP.')
         return [float(x) for x in read.replace('\x00', '').split(',')]
 
-    # TODO: time constant
     @Feat(limits=(0, 29, 1))
     def time_constant_integer(self):
         """
@@ -116,9 +180,7 @@ class SignalRecovery7265(MessageBasedDriver):
         """
         return self.write('TC{}'.format(integer))
 
-    # DERP DERP DERP test
-
-    @Feat(values=time_constants)
+    @Feat(values=TIME_CONSTANTS)
     def time_constant(self):
         """
         Returns current time constant setting (in seconds).
@@ -128,9 +190,10 @@ class SignalRecovery7265(MessageBasedDriver):
     @time_constant.setter
     def time_constant(self, time_const):
         """
-        Sets the current time constant setting (in seconds).
+        Not implemented, not a built in functionality.
         """
-        return self.write('TC.{}'.format(time_const))
+        print('Error: invalid operation, cannot directly set TC')
+        return 0
 
     @Feat()
     def frequency(self):
@@ -154,11 +217,61 @@ class SignalRecovery7265(MessageBasedDriver):
         """
         return self.write('OF.{}'.format(frequency))
 
-    # TODO: gain
+    @Feat(values=AC_GAINS)
+    def gain(self):
+        """
+        Read current AC gain (dB).
+        """
+        return int(self.query('ACGAIN'))
 
-    # TODO: sensitivity
+    @gain.setter
+    def gain(self, gain_value):
+        """
+        Set current AC gain (dB)
+        """
+        return self.write('ACGAIN{}'.format(gain_value))
 
-    @Feat(values=int_ext_refs)
+    @Feat(values=SENSITIVITIES)
+    def sensitivity(self):
+        """
+        Gets sensitivity according to the SENSITIVITIES table.
+        """
+        return int(self.query('SEN'))
+
+    # @Feat(values=SENSITIVITIES)
+    # def sensitivity_int(self):
+    #     """
+    #     Returns integer value of sensitivity as described by SENSITIVITIES.
+    #     """
+    #     return int(self.query('SEN'))
+
+    @sensitivity.setter
+    def sensitivity(self, sen_it):
+        """
+        Sets value of sensitivity as described by SENSITIVITIES.
+        """
+        return self.write('SEN{}'.format(sen_it))
+
+    @Action()
+    def autosensitivity(self):
+        """
+        Runs an autosensitivity operation.
+
+        The instrument adjusts its full-scale sensitivity so that the magnitude
+        output lies between 30-90 percent of full-scale
+        """
+        a = self.write('AS')
+        sleep(7.5)  # wait for operation to complete
+        return a
+
+    @Action()
+    def autogain(self):
+        """
+        Set current AC gain to automatic.
+        """
+        return self.write('AUTOMATIC1')
+
+    @Feat(values=INT_EXT_REF)
     def int_ext_ref(self):
         """
         Check if lockin is internally or externally referenced
@@ -172,41 +285,54 @@ class SignalRecovery7265(MessageBasedDriver):
         """
         return self.write('IE {}'.format(value))
 
-
 if __name__ == '__main__':
-    with SignalRecovery7265('GPIB0::7::INSTR') as inst:
+    with SignalRecovery7265.via_gpib(7) as inst:
         print('The instrument identification is ' + inst.idn)
 
         print('Testing signal readings')
-        print('Signal X component is ' + str(inst.x) + 'V')
-        print('Signal Y component is ' + str(inst.y) + 'V')
-        print('Signal magnitude is ' + str(inst.magnitude) + 'V')
-        print('Signal phase is ' + str(inst.phase) + 'degrees')
+        print('Signal X: {}V'.format(inst.x))
+        print('Signal Y: {}V'.format(inst.y))
+        print('Signal magnitude: {}V'.format(inst.magnitude))
+        print('Signal phase: {}degrees'.format(inst.phase))
 
         print('Testing full quadrature readings')
-        print('XY measurement: ' + str(inst.xy))
-        print('magnitude and phase measurement: ' + str(inst.mag_phase))
+        print('X,Y: {}V'.format(list(inst.xy)))
+        print('Magnitude, Phase: {}'.format(list(inst.mag_phase)))
 
-        print('What\'s the frequency, Kenneth?')
-        print('Reference frequency: ' + str(inst.frequency) + 'Hz')
+        print('Testing frequency code')
+        print('Ref f: {}Hz'.format(inst.frequency))
         inst.oscillator_freq = 137.0
-        print('Reference frequency: ' + str(inst.frequency) + 'Hz')
+        print('Ref f: {}Hz'.format(inst.frequency))
         inst.oscillator_freq = 17
-        print('Reference frequency: ' + str(inst.frequency) + 'Hz')
+        print('Ref f: {}Hz'.format(inst.frequency))
         #
         print('Internal External Reference check')
-        print('Internal/ext reference: ' + inst.int_ext_ref)
+        print('Internal/ext reference: {}'.format(inst.int_ext_ref))
         inst.int_ext_ref = 'ext'
-        print('Internal/ext reference: ' + inst.int_ext_ref)
+        print('Internal/ext reference: {}'.format(inst.int_ext_ref))
         inst.int_ext_ref = 'int'
-        print('Internal/ext reference: ' + inst.int_ext_ref)
+        print('Internal/ext reference: {}'.format(inst.int_ext_ref))
 
         print('Time constant check')
-        print('Int TC: ' + str(inst.time_constant_integer))
-        print('TC (sec): ' + str(inst.time_constant))
+        print('Int TC: {}'.format(inst.time_constant_integer))
+        print('TC (sec): {}s'.format(inst.time_constant))
         inst.time_constant_integer = 15
-        print('Int TC: ' + str(inst.time_constant_integer))
-        print('TC (sec): ' + str(inst.time_constant))
-        inst.time_constant = 100e-3
-        print('Int TC: ' + str(inst.time_constant_integer))
-        print('TC (sec): ' + str(inst.time_constant))
+        print('Int TC: {}'.format(inst.time_constant_integer))
+        print('TC (sec): {}s'.format(inst.time_constant))
+        inst.time_constant_integer = 10
+        print('Int TC: {}'.format(inst.time_constant_integer))
+        print('TC (sec): {}s'.format(inst.time_constant))
+
+        print('AC Gain Check')
+        print('AC Gain: {}'.format(inst.gain))
+        inst.gain = '30dB'
+        print('AC Gain: {}'.format(inst.gain))
+        inst.autogain
+        print('AC Gain: {}'.format(inst.gain))
+
+        print('Sensitivity Check')
+        print('Sen: {}'.format(inst.sensitivity))
+        inst.sensitivity = '2e-8V'
+        print('Sen: {}'.format(inst.sensitivity))
+        inst.autosensitivity()
+        print('Sen: {}'.format(inst.sensitivity))
