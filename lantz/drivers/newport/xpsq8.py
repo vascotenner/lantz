730f015a06f09c56b2b3db1f145220d6d1034d06
Kevin Miao <kmiao@uchicago.edu>
2016-02-05 17:28:20 -0600

    adds additional XPS commands

--- lantz/drivers/newport/xpsq8.py
+++ lantz/drivers/newport/xpsq8.py
@@ -12,9 +12,12 @@
 """
 
 from lantz.driver import Driver
+from lantz import Feat, DictFeat, Action
 import XPS_Q8_drivers
 
 class XPSQ8(Driver):
+
+    channels = {'X', 'Y', 'Z'}
 
     def __init__(self, address, port=5001, timeout=20.0):
         super(XPSQ8, self).__init__()
@@ -24,20 +27,31 @@ class XPSQ8(Driver):
             self.log_error("Failed to establish XPS connection at {0}:{1}", address, port)
             # how do we proceed gracefully after this?
 
-
     @Action()
     def reboot(self):
         self._xps.Reboot(self._socket_id)
         return
 
-    @Feat()
-    def abs_position(self):
-        pass
+    @DictFeat()
+    def travel_limits(self, channel):
+        retval = self._xps.PositionerUserTravelLimitsGet(self._socket_id, channel)
 
     @Action()
+    def home(self, channel):
+        retval = self._xps.GroupHomeSearch(self._socket_id, channel)
+
+    @DictFeat()
+    def abs_position(self, channel):
+        retval = self._xps.GroupPositionCurrentGet(self._socket_id, channel, 1)
+        error, curpos = retval
+        if error:
+            raise ValueError
+        return float(curpos)
+
+    @abs_position.setter
     def abs_position(self, channel, position):
-        pass
+        retval = self._xps.GroupMoveAbsolute(self._socket_id, channel, [position])
 
     @Action()
     def rel_position(self, channel, dposition):
-        pass
+        retval = self._xps.GroupMoveRelative(self._socket_id, channel, [dposition])
