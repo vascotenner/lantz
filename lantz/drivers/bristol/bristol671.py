15330d3abf602f4555dd9f17228e96f8d673c056
AlexBourassa <alexandre.bourassa.mail@gmail.com>
2017-05-28 18:21:45 -0500

    Updated Bristol Drivers  Nwo working

--- lantz/drivers/bristol/bristol671.py
+++ lantz/drivers/bristol/bristol671.py
@@ -1,7 +1,36 @@
-from lantz.messagebased import MessageBasedDriver
+from lantz.driver import Driver
 from lantz import Feat, Action
 
-class Bristol671(MessageBasedDriver):
+from telnetlib import Telnet
+
+class Bristol671(Driver):
+
+    def __init__(self, ip, port=23, timeout=1):
+        super().__init__()
+        self.con_args = [ip,port,timeout]
+
+    def initialize(self):
+        self.resource = Telnet(*self.con_args)
+        self.clear_read_buffer()
+    
+    def clear_read_buffer(self, timeout=0.1):
+        while self.read(timeout=timeout) != '':
+            pass
+
+    def finalize(self):
+        self.resource.close()
+
+    def write(self, cmd, write_termination='\r\n'):
+        return self.resource.write(bytes(cmd+write_termination, 'ascii'))
+
+    def read(self, read_termination='\r\n', timeout=1):
+        byte_ans = self.resource.read_until(bytes(read_termination, 'ascii'), timeout)
+        ans = byte_ans.decode('ascii')
+        return ans.strip(read_termination)
+
+    def query(self, cmd, write_termination='\r\n', read_termination='\r\n'):
+        self.write(cmd, write_termination=write_termination)
+        return self.read(read_termination=read_termination)
 
     @Feat()
     def idn(self):
@@ -11,7 +40,7 @@ class Bristol671(MessageBasedDriver):
     def wavelength(self):
         return float(self.query(':MEAS:WAV?'))
 
-    @Feat(units='GHz')
+    @Feat(units='THz')
     def frequency(self):
         return float(self.query(':MEAS:FREQ?'))
 
