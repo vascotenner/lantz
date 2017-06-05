f60f5711dd7b2ac2df361394103c20c3285fd388
Kevin Miao <kmiao@uchicago.edu>
2017-06-05 17:50:33 -0500

    adds scan control and updates error handling

--- lantz/drivers/toptica/dlcpro.py
+++ lantz/drivers/toptica/dlcpro.py
@@ -21,34 +21,45 @@ class DLC(MessageBasedDriver):
                 'read_termination': '\n',
             }
         }
+
         def initialize(self):
             super().initialize()
-            ## Clear the welcome message
-            for i in range(5): print(self.resource.read_raw())
-            self._read = self.read
-            self.read  = self.parsed_read
+            # clear welcome message
+            for _ in range(5):
+                self.resource.read_raw()
+            return
 
-        def parsed_read(self, *args, **kwargs):
-            ans = self._read(*args, **kwargs)
-            if ans[0] == '>': # Take away the prompt "> "
-                ans = ans[2:]
-            return ans.strip('"') # Take away the quotation for strings
+        def set_param(self, param_name, value):
+            cmd = "(param-set! '{} {})".format(param_name, value)
+            self.write(cmd)
+            retval = self.read()
+            retval = retval.lstrip('> ')
+            if retval.startswith('Error: '):
+                retval = retval.lstrip('Error: ')
+                retparts = retval.split()
+                errcode, errmsg = int(retparts[0]), ' '.join(retparts[1:])
+                errcode = int(self.read())
+                raise DLCException("Error {}: {}".format(errcode, errmsg))
+            return retval
 
-        def get(self, param):
-            return self.query("(param-ref '{})".format(param))
+        def get_param(self, param_name):
+            cmd = "(param-ref '{})".format(param_name)
+            self.write(cmd)
+            retval = self.read()
+            retval = retval.lstrip('> ')
+            if retval.startswith('Error: '):
+                retval = retval.lstrip('Error: ')
+                retparts = retval.split()
+                errcode, errmsg = int(retparts[0]), ' '.join(retparts[1:])
+                raise DLCException("Error {}: {}".format(errcode, errmsg))
+            return retval
 
-        def set(self, param, val):
-            err = self.query("(param-set! '{} {})".format(param, val))
-            if err != '0':
-                raise Exception("(param-set! '{} {}) returned an error code of '{}'".format(param, val, err))
-            return err
-
-        @Feat
+        @Feat()
         def idn(self):
             """
             Returns DLCpro
             """
-            return self.get("system-type")
+            return self.get_param("system-type")
 
 
         ##------------------------
@@ -57,43 +68,67 @@ class DLC(MessageBasedDriver):
 
         @Feat(values={True: '#t', False: '#f'})
         def output(self):
-            return self.get('laser1:dl:cc:enabled')
+            return self.get_param('laser1:dl:cc:enabled')
 
         @output.setter
         def output(self, val):
-            return self.set('laser1:dl:cc:enabled', val)
+            return self.set_param('laser1:dl:cc:enabled', val)
 
         @Feat(values={True: '#t', False: '#f'})
         def feedforward_enabled(self):
-            return self.get('laser1:dl:cc:feedforward-enabled')
+            return self.get_param('laser1:dl:cc:feedforward-enabled')
 
         @feedforward_enabled.setter
         def feedforward_enabled(self, val):
-            return self.set('laser1:dl:cc:feedforward-enabled', val)
+            return self.set_param('laser1:dl:cc:feedforward-enabled', val)
 
         @Feat(units='mA')
         def current(self):
-            return self.get('laser1:dl:cc:current-set')
+            return self.get_param('laser1:dl:cc:current-set')
 
         @current.setter
         def current(self, val):
-            return self.set('laser1:dl:cc:current-set', val)
+            return self.set_param('laser1:dl:cc:current-set', val)
 
         @Feat(units='mA')
         def current_offset(self):
-            return self.get('laser1:dl:cc:current-offset')
+            return self.get_param('laser1:dl:cc:current-offset')
 
         @current_offset.setter
         def current_offset(self, val):
-            return self.set('laser1:dl:cc:current-offset', val)
+            return self.set_param('laser1:dl:cc:current-offset', val)
 
         @Feat(units='mA/V')
         def feedforward_factor(self):
-            return self.get('laser1:dl:cc:feedforward-factor')
+            return self.get_param('laser1:dl:cc:feedforward-factor')
 
         @feedforward_factor.setter
         def feedforward_factor(self, val):
-            return self.set('laser1:dl:cc:feedforward-factor', val)
+            return self.set_param('laser1:dl:cc:feedforward-factor', val)
+
+        @Feat()
+        def external_input_signal(self):
+            return int(self.get_param('laser1:dl:cc:external-input:signal'))
+
+        @external_input_signal.setter
+        def external_input_signal(self, val):
+            return self.set_param('laser1:dl:cc:external-input:signal', int(val))
+
+        @Feat()
+        def external_input_factor(self):
+            return float(self.get_param('laser1:dl:cc:external-input:factor'))
+
+        @external_input_factor.setter
+        def external_input_factor(self, val):
+            return self.set_param('laser1:dl:cc:external-input:factor', float(val))
+
+        @Feat(values={True: '#t', False: '#f'})
+        def external_input_enabled(self):
+            return self.get_param('laser1:dl:cc:external-input:enabled')
+
+        @external_input_enabled.setter
+        def external_input_enabled(self, val):
+            return self.set_param('laser1:dl:cc:external-input:enabled', val)
 
 
         ##------------------------
@@ -102,24 +137,81 @@ class DLC(MessageBasedDriver):
 
         @Feat(values={True: '#t', False: '#f'})
         def piezo_enabled(self):
-            return self.get('laser1:dl:pc:enabled')
+            return self.get_param('laser1:dl:pc:enabled')
 
         @piezo_enabled.setter
         def piezo_enabled(self, val):
-            return self.set('laser1:dl:pc:enabled', val)
+            return self.set_param('laser1:dl:pc:enabled', val)
 
         @Feat(units='V')
         def piezo_vmax(self):
-            return self.get('laser1:dl:pc:voltage-max')
+            return self.get_param('laser1:dl:pc:voltage-max')
 
         @Feat(units='V')
         def piezo_vmin(self):
-            return self.get('laser1:dl:pc:voltage-min')
+            return self.get_param('laser1:dl:pc:voltage-min')
 
         @Feat(units='V')
         def piezo_voltage(self):
-            return self.get('laser1:dl:pc:voltage-set')
+            return self.get_param('laser1:dl:pc:voltage-set')
 
         @piezo_voltage.setter
         def piezo_voltage(self, val):
-            return self.set('laser1:dl:pc:voltage-set', val)
+            return self.set_param('laser1:dl:pc:voltage-set', val)
+
+        ##------------------------
+        ##    Scan Control
+        ##------------------------
+
+        @Feat(values={True: '#t', False: '#f'})
+        def scan_enabled(self):
+            return self.get_param('laser1:scan:enabled')
+
+        @scan_enabled.setter
+        def scan_enabled(self, val):
+            return self.set_param('laser1:scan:enabled', val)
+
+        @Feat()
+        def scan_channel(self):
+            return int(self.get_param('laser1:scan:output-channel'))
+
+        @scan_channel.setter
+        def scan_channel(self, val):
+            return self.set_param('laser1:scan:output-channel', int(val))
+
+        @Feat()
+        def scan_amplitude(self):
+            return float(self.get_param('laser1:scan:amplitude'))
+
+        @scan_amplitude.setter
+        def scan_amplitude(self, val):
+            return self.set_param('laser1:scan:amplitude', float(val))
+
+        @Feat()
+        def scan_offset(self):
+            return float(self.get_param('laser1:scan:offset'))
+
+        @scan_offset.setter
+        def scan_offset(self, val):
+            return self.set_param('laser1:scan:offset', float(val))
+
+        @Feat(units='Hz')
+        def scan_frequency(self):
+            return self.get_param('laser1:scan:frequency')
+
+        @scan_frequency.setter
+        def scan_frequency(self, val):
+            return self.set_param('laser1:scan:frequency', val)
+
+        @Feat(values={'sine': 0, 'triangle': 1})
+        def scan_signal_type(self):
+            return int(self.get_param('laser1:scan:signal-type'))
+
+        @scan_signal_type.setter
+        def scan_signal_type(self, val):
+            return self.set_param('laser1:scan:signal-type', val)
+
+
+
+class DLCException(Exception):
+    pass
