94275b5a2317ce96b961f0d5460c5dc6920cd051
AlexBourassa <alexandre.bourassa.mail@gmail.com>
2017-07-06 09:55:53 -0500

    Goniometer drivers

--- lantz/drivers/arduino/goniometer.py
+++ lantz/drivers/arduino/goniometer.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-    lantz.drivers.arduino.goniometer
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	lantz.drivers.arduino.goniometer
+	~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    Lantz interface to the arduino controlling the goniometer
+	Lantz interface to the arduino controlling the goniometer
 
-    Authors: Alexandre Bourassa
-    Date: 3/28/2017
+	Authors: Alexandre Bourassa
+	Date: 3/28/2017
 
 """
 
@@ -19,71 +19,85 @@ import time
 
 class Goniometer(MessageBasedDriver):
 
-    comm_delay = 1
+	comm_delay = 1
 
-    DEFAULTS = {
-        'ASRL': {
-            'write_termination': '\n',
-            'read_termination': '\r\n',
-            'baud_rate': 9600,
-            'timeout': None,
-        }
-    }
+	DEFAULTS = {
+		'ASRL': {
+			'write_termination': '\n',
+			'read_termination': '\r\n',
+			'baud_rate': 9600,
+			'timeout': 2000,
+		}
+	}
 
-    errors = {
-     -1:  'tetha or phi were out of bounds',
-     -2:  'alpha or beta were out of bounds',
-     -3:  'attempted rotateTo while run_state != 0',
-     -4:  'period is out of bound',
-    -10:  'Unknown cmd',
-    -11:  'other error',
-    -50:  'no error',
-    }
+	errors = {
+	 -1:  'theta or phi were out of bounds',
+	 -2:  'alpha or beta were out of bounds',
+	 -3:  'attempted rotateTo while run_state != 0',
+	 -4:  'period is out of bound',
+	 -9:  'R is out of bounds',
+	-10:  'Unknown cmd',
+	-11:  'other error',
+	-50:  'no error',
+	}
 
-    def check_error(self, err):
-        if type(err) == int and err<0:
-            if err != -50:
-                raise Exception(self.errors[int(err)])
-            else:
-                print("No Error")
+		
+	def initialize(self):
+		super().initialize()
+		time.sleep(2)
+		self.query('stop')
+		time.sleep(2)
+	
+	def check_error(self, err):
+		if err != -50:
+			raise Exception(self.errors[err])
+		else:
+			print("Success")
+	
+	def query(self, cmd):
+		self.write(cmd)
+		ans = self.read()
+		err = int(self.read())
+		self.check_error(err)
+		return ans
+	
+	@Feat(limits=(70, 110, 0.01))
+	def theta(self):
+		return float(self.query('theta?'))
 
-    def write_and_check(self, *args, **kwargs):
-        self.check_error(self.query(*args, **kwargs))
+	@theta.setter
+	def theta(self, val):
+		float(self.query('theta {}'.format(val)))
 
-    def query(self, cmd):
-        super().write(cmd)
-        time.sleep(self.comm_delay)
-        ans = super().read()
-        self.check_error(ans)
-        ans = super().read()
-        return ans
+	@Feat(limits=(30, 135, 0.01))
+	def phi(self):
+		return float(self.query('phi?'))
 
-    @Feat()
-    def theta(self):
-        return self.query('theta?')
+	@phi.setter
+	def phi(self, val):
+		return float(self.query('phi {}'.format(val)))
+		
+	@Feat() # units = mm
+	def R(self):
+		return float(self.query('getr'))
+	
+	@R.setter
+	def R(self, val):
+		return self.query('abs {}'.format(val))
+	
+	@Feat()
+	def state(self):
+		return self.query('state?')
+		
+	@Feat()
+	def stop(self):
+		self.query('stop')
+		
+	@Feat()
+	def period(self):
+		return self.query('period?')
+	
+	@period.setter
+	def period(self, val):
+		return self.query('pperiod {}'.format(val))
 
-    @theta.setter
-    def theta(self, val):
-        self.query('theta {}'.format(val))
-
-    @Feat()
-    def phi(self):
-        return self.query('phi?')
-
-    @phi.setter
-    def theta(self, val):
-        self.query('phi {}'.format(val))
-
-
-    @Action()
-    def zero(self):
-        self.write('zero 0')
-        self.write('zero 1')
-
-    @Feat()
-    def state(self):
-        return self.query('state?')
-
-    @Action()
-    def stop(self):
-        self.write('stop')
