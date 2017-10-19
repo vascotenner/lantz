e8939394a855c256f1ded14ce69537d2318355bc
justcamdig <32750615+justcamdig@users.noreply.github.com>
2017-10-19 17:20:06 -0500

    edited lakeshore475.py

--- lantz/drivers/lakeshore/lakeshore475.py
+++ lantz/drivers/lakeshore/lakeshore475.py
@@ -6,3 +6,8 @@ class Lakeshore475(MessageBasedDriver):
     @Feat(units='gauss')
     def field(self):
         return self.query('RDGFIELD?')
+    
+    @Feat(units='Hz')
+    def frequency(self):
+        return self.query('RDGFRQ?')
+
