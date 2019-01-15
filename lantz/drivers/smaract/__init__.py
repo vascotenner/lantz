e7ba2ba54220232e1f4cfe501c6189e921eedc80
Vasco Tenner <v.t.tenner@vu.nl>
2019-01-15 11:15:48 +0100

    Disable auto import from smaract.

---
+++ lantz/drivers/smaract/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+    lantz.drivers.smaract
+    ~~~~~~~~~~~~~~~~~~~~~
+
+    :company: Smaract
+    :description: Piezo stages and positioners.
+    :website: http://www.smaract.com/
+
+    ---
+
+    :copyright: 2015, see AUTHORS for more details.
+    :license: GPLv3,
+
+"""
+
+#from .smaract_motion_SCU import SCU
+#from .MCS2 import MCS2_Bare
+
+#__all__ = ['SCU', 'MCS2_Bare']
