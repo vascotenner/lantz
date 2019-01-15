c095aceec5f7cbf48397eb1d8941a3127671729b
vascotenner <v.t.tenner@vu.nl>
2019-01-15 16:46:35 +0100

    Driver for MCS2 smaract stage

--- lantz/drivers/smaract/MCS2.py
+++ lantz/drivers/smaract/MCS2.py
@@ -14,24 +14,32 @@
 """
 
 import ctypes as ct
-from ctypes import c_uint, c_int, byref
-from lantz.errors import InstrumentError
-from lantz.foreign import LibraryDriver
-from lantz.feat import Feat
-from lantz.action import Action
-
-from lantz import Q_, ureg
-from lantz.processors import convert_to
-from lantz.drivers.motion import MotionAxisMultiple, MotionControllerMultiAxis, BacklashMixing
-import time
-import numpy as np
+from ctypes import byref
 
 import lantz.drivers.smaract.SmarActControlConstants as SmarActControlConstants
+from lantz.action import Action
+from lantz.drivers.motion import MotionAxisMultiple, MotionControllerMultiAxis
+from lantz.errors import InstrumentError
+from lantz.feat import Feat
+from lantz.foreign import LibraryDriver
+
+
+def gen_dict_from_prefix(namespace, prefix):
+    return {getattr(namespace, constant): constant[len(prefix):]
+     for constant in dir(namespace)
+     if constant.startswith(prefix)}
+
+
+def invert_dict(d):
+    return {val: key for key,val in d.items() }
+
 
 # Generate dict of error codes from altered header file
-_ERRORS = {getattr(SmarActControlConstants, constant): constant[13:]
-           for constant in dir(SmarActControlConstants)
-           if constant[:13] == 'SA_CTL_ERROR_'}
+_ERRORS = gen_dict_from_prefix(SmarActControlConstants, 'SA_CTL_ERROR_')
+
+_MOVE_MODES =  gen_dict_from_prefix(SmarActControlConstants, 'SA_CTL_MOVE_MODE_')
+
+_STATES = gen_dict_from_prefix(SmarActControlConstants, 'SA_CTL_CH_STATE_BIT_')
 
 # List of functions that should not be checked for errorcodes
 _IGNORE_ERR = []
@@ -43,6 +51,9 @@ SA_CTL_StreamHandle_t = ct.c_uint32
 SA_CTL_RequestID_t = ct.c_uint8
 SA_CTL_PropertyKey_t = ct.c_uint32
 SA_CTL_Result_t = ct.c_uint32
+size_t = ct.c_size_t
+idx = ct.c_int8
+
 
 class MCS2_Find_devices(LibraryDriver):
     """ Driver for MCS controller
@@ -53,8 +64,16 @@ class MCS2_Find_devices(LibraryDriver):
     def _add_types(self):
         self.lib.SA_CTL_FindDevices.argtypes = [ct.c_char_p, ct.c_char_p, ct.POINTER(ct.c_size_t)]
         self.lib.SA_CTL_Open.argtypes = [ct.POINTER(SA_CTL_DeviceHandle_t), ct.c_char_p, ct.c_char_p]
-        pass
-        # self.lib.XC_OpenCamera.argtypes = [ct.c_uint]
+        self.lib.SA_CTL_Move.argtypes = [SA_CTL_DeviceHandle_t, ct.c_int8, ct.c_int64, SA_CTL_TransmitHandle_t]
+
+        self.lib.SA_CTL_GetProperty_i64.argtypes = [SA_CTL_DeviceHandle_t, idx, SA_CTL_PropertyKey_t, ct.POINTER(ct.c_int64), (size_t)]
+        self.lib.SA_CTL_SetProperty_i64.argtypes = [SA_CTL_DeviceHandle_t, idx, SA_CTL_PropertyKey_t, ct.c_int64]
+
+        self.lib.SA_CTL_GetProperty_i32.argtypes = [SA_CTL_DeviceHandle_t, idx, SA_CTL_PropertyKey_t, ct.POINTER(ct.c_int32), (size_t)]
+        self.lib.SA_CTL_SetProperty_i32.argtypes = [SA_CTL_DeviceHandle_t, idx, SA_CTL_PropertyKey_t, ct.c_int32]
+
+        self.lib.SA_CTL_GetProperty_s.argtypes = [SA_CTL_DeviceHandle_t, idx, SA_CTL_PropertyKey_t,
+                                                    ct.c_char_p, ct.POINTER(size_t)]
 
     def _return_handler(self, func_name, ret_value):
         if self.lib.__getattribute__(func_name).restype != ct.c_void_p and (func_name not in _IGNORE_ERR):
@@ -67,14 +86,13 @@ class MCS2_Find_devices(LibraryDriver):
 
     @Action()
     def find_devices(self):
-        raise NotImplementedError
-        #deviceList = (ct.c_char_p * 1000)()
-        #ioDeviceListLen = len(deviceList)
-        deviceList = ""
+        deviceList = bytes("".join(1024 * [" "]), encoding='utf8')
         ioDeviceListLen = ct.c_size_t(len(deviceList))
-        self.lib.SA_CTL_FindDevices("", deviceList, byref(ioDeviceListLen))
+        self.lib.SA_CTL_FindDevices(bytes("", encoding='utf8'), deviceList, byref(ioDeviceListLen))
+        return str(deviceList)[:ioDeviceListLen.value]
 
 class MCS2_Bare(MCS2_Find_devices):
+    """Only consists direct wrappers around library"""
     def __init__(self, device_locator=None, *args, **kwargs):
         """ Set default settings for device
 
@@ -84,8 +102,6 @@ class MCS2_Bare(MCS2_Find_devices):
                 'network:sn:<serial>'
                 'network:<ip>'
         """
-
-
         super().__init__(*args, **kwargs)
         self.device_locator = device_locator
         self.dHandle = ct.c_uint32()
@@ -97,112 +113,110 @@ class MCS2_Bare(MCS2_Find_devices):
         Initialize the piezo stage
         context will be passed to self.dHandle
         """
-        self.lib.SA_CTL_Open(byref(self.dHandle), self.device_locator, "");
-#        self.lib.SA_OpenSystem(byref(self.dHandle), self.device_locator, 'sync')
+        self.lib.SA_CTL_Open(byref(self.dHandle), self.device_locator, "")
 
     def finalize(self):
         self.lib.SA_CTL_Close(self.dHandle)
 
-#TODO
-class TODO():
-    def setchannelproperty(self, channel_idx, selector, subselector, prop, propvalue):
+    @Action()
+    def move_channel(self, channel, value, tHandle=0):
+        self.lib.SA_CTL_Move(self.dHandle, channel, int(value), tHandle)
+
+    def getproperty_i64(self, channel, key):
+        val = ct.c_int64()
+        self.lib.SA_CTL_GetProperty_i64(self.dHandle, channel, key, byref(val), 0)
+        return val.value
+
+    def setproperty_i64(self, channel, key, val):
+        self.lib.SA_CTL_SetProperty_i64(self.dHandle, channel, key, val)
+
+    def getproperty_i32(self, channel, key):
+        val = ct.c_int32()
+        self.lib.SA_CTL_GetProperty_i32(self.dHandle, channel, key, byref(val), 0)
+        return val.value
+
+    def setproperty_i32(self, channel, key, val):
+        self.lib.SA_CTL_SetProperty_i32(self.dHandle, channel, key, val)
+
+    def getproperty_s(self, channel, key):
+        val = bytes("".join(128 * [" "]), encoding='utf8')
+        valLen = ct.c_size_t(len(val))
+        self.lib.SA_CTL_GetProperty_s(self.dHandle, channel, key, val, byref(valLen))
+        return str(val)[:valLen.value]
+
+class MCS2_Minimum(MCS2_Bare):
+    '''Contains some minimum functionallity in order to use with MCS2 and motion classes'''
+    @Action()
+    def read_position(self, channel):
+        return self.getproperty_i64(channel, SmarActControlConstants.SA_CTL_PKEY_POSITION)
+
+    def read_move_mode(self, channel):
+        return self.getproperty_i32(channel, SmarActControlConstants.SA_CTL_PKEY_MOVE_MODE)
+
+    def write_move_mode(self, channel, moveMode):
+        self.setproperty_i32(channel, SmarActControlConstants.SA_CTL_PKEY_MOVE_MODE, moveMode)
+
+    def channel_states(self, channel):
+        state = self.getproperty_i32(channel, SmarActControlConstants.SA_CTL_PKEY_CHANNEL_STATE)
+        states = [val for key, val in _STATES.items() if (key & state)]
+        return states
+
+    @Feat()
+    def idn(self):
+        return self.getproperty_s(0, SmarActControlConstants.SA_CTL_PKEY_DEVICE_NAME)
+
+
+class MCS2_Axis(MotionAxisMultiple):
+    @Feat(values=invert_dict(_MOVE_MODES))
+    def move_mode(self):
+        return self.parent.read_move_mode(self.num)
+
+    @move_mode.setter
+    def move_mode(self, moveMode):
+        self.parent.write_move_mode(self.num, moveMode)
+
+    @Feat(units='pm')
+    def position(self):
+        return self.parent.read_position(self.num)
+
+    @position.setter
+    def position(self, pos):
         """
-        SetChannelProperty(self, channel_idx, selector, subselector, prop, propvalue):
-        sets channel properties to different values
+        Waits until movement is done if self._config['wait_until_done'] = True.
 
-        Args:
-            channel_idx(int): choose a channel(each corresponds a linear translation axis of a stage or a rotational axis)
-            selector(int): broad category
-            subselector(int):narrower category
-            prop(int):property to change
-            propvalue(int): value to change it too
-
-            here are some of the more usefull properties to change and corresponding subselectors
-
-            selector 1:general
-                subselector 2:Low Vibration mode
-                    prop 1:Operation mode
-                        propvalues 0,1:enabled,disabled
-
-
-
-
+        :param pos: new position
         """
+        wait = self.wait_time if self._config['wait_until_done'] else None
+        self._set_position(pos, wait=wait)
 
-        self.dll.SA_SetChannelProperty_S(self.MCS_HANDLE, channel_idx, self.dll.SA_EPK(selector, subselector, prop), propvalue)
-
-    def get_number_of_channels(self):
-        """"
-        saves the number of detected channels
-
-
+    def _write_position(self, pos):
         """
-        NUMOFCHANNELS = c_uint()
-        error = self.dll.SA_GetNumberOfChannels(self.MCS_HANDLE, byref(NUMOFCHANNELS))
-        self.num_of_chan = NUMOFCHANNELS.value
-        return NUMOFCHANNELS
-
-    def go_to_angle(self,channel_idx,angle,revolution,holdtime):
+        Move stage to a certain position
+        :param pos: New position
         """
+        self.parent.move_channel(self.num, pos)
 
-        :param channel_idx: channel to rotate
-        :param angle: angle to move to in micro radians, if the sensor is on( change with set_power_mode(channel_idx,mode)
-                        stored in self.power_mode), will be interpreted as a move to a certain angle compared to the
-                        current zero point if the sensor power_mode is disabled it will rotate with respect
-                        to the current angle
-        :param revolution: number of 360 degree revolutions
-        :param holdtime:
-        :return:
-        """
+    @Feat()
+    def states(self):
+        return self.parent.channel_states(self.num)
 
-        if self.power_mode==0:
-            self.dll.SA_gotoAngleRelative_S(self.MCS_HANDLE,channel_idx,c_uint(angle),c_int(revolution),c_int(holdtime))
-        else:
-            self.dll.SA_GotoAngleAbsolute_S(self.MCS_HANDLE,channel_idx,c_uint(angle),c_int(revolution),c_int(holdtime))
+    @Feat()
+    def motion_done(self):
+        if 'ACTIVELY_MOVING' in self.states:
+            return False
+        return True
 
-    def rotate_relative(self, channel_idx, angle,revolution, holdtime):
-        self.dll.SA_gotoAngleRelative_S(self.MCS_HANDLE, channel_idx, c_uint(angle), c_int(revolution), c_int(holdtime))
-
-    def set_power_mode(self,mode):
-        """
-        changes the power_mode of the sensor of a certain channel
-
-        #power supply operation mode may be important during measurements, as the light of the sensor will influence
-        the measurements, disable the sensor when you are taking a measurement, power_save is somewhere in the middle
-        with less sensor light(also less heat produced) but presumably either less accuracy or slower adjustments,
-        can be usefull for allignment with smaract stages
-
-        moving to an absolute position requires the power_mode to be enabled
+    @Feat()
+    def present(self):
+        return 'SENSOR_PRESENT' in self.states
 
 
-        :param mode: which operation mode
-                    0: disabled
-                        1: enabled
-                        2: powersave
-
-        """
-        # for i in range(self.numofchan):
-        #     channel_idx=c_uint(i)
-        self.dll.SA_SetSensorEnabled_S(self.MCS_HANDLE,c_uint(mode))
-        self.power_mode=mode
-
-    def get_power_mode(self):
-        """
-        asks the current powermode and stores it in self.powermode
-        :return:
-        """
-        getmode=c_uint()
-        self.dll.SA_GetSensorEnabled_S(self.MCS_HANDLE,byref(getmode))
-
-        self.powermode=getmode.value
-        return self.powermode
-
-
-class MCS2(MCS2_Bare, MotionControllerMultiAxis):
-    """ Driver for SCU controller with multiple axis
+class MCS2(MCS2_Minimum, MotionControllerMultiAxis):
+    """ Driver for MCS controller with multiple axis
 
     """
-    def __init__(self, device_locator=None, dll='MCSControl', *args, **kwargs):
+
+    def __init__(self, device_locator=None, *args, **kwargs):
         """ Set default settings for device
 
         :param device_locator: string, eg:
@@ -210,26 +224,13 @@ class MCS2(MCS2_Bare, MotionControllerMultiAxis):
                 'usb:ix:<n>'
                 'network:sn:<serial>'
                 'network:<ip>'
-        :param dll: name of dll or so object. Should be found by ld
         """
-        super().__init__(*args, **kwargs)
-        self.stage = SmaractStage
+        super().__init__(device_locator, *args, **kwargs)
 
     def initialize(self):
         super().initialize()
+        print(self.idn)
         self.detect_axis()
-
-    def query(self, command, *, send_args=(None, None), recv_args=(None, None)):
-        return MotionControllerMultiAxis.query(self, ':{}'.format(command),
-                                 send_args=send_args, recv_args=recv_args)
-
-    def write(self, command, *args, **kwargs):
-        return MotionControllerMultiAxis.write(self,':{}'.format(command),
-                                 *args, **kwargs)
-
-    @Feat()
-    def idn(self):
-        return self.parse_query('I', format='I{:s}')
 
     @Action()
     def detect_axis(self):
@@ -237,167 +238,31 @@ class MCS2(MCS2_Bare, MotionControllerMultiAxis):
 
         The detection stops as soon as an empty controller is found.
         """
-        pass
+        num = 0
+        while True:
+            axis = MCS2_Axis(parent=self, num=num,
+                          id='{}.axis[{}]'.format(self.idn, num))
+            if not axis.present:
+                return
 
-#TODO:
-class MotionAxis(MotionAxisMultiple, BacklashMixing):
-    def __del__(self):
-        self.parent = None
-        self.num = None
+            self.axes.append(axis)
 
-    def query(self, command, *, send_args=(None, None), recv_args=(None, None)):
-        return self.parent.query('{:d}{}'.format(self.num, command),
-                                 send_args=send_args, recv_args=recv_args)
+            # update units of axis NOT WORKING on 20190109
+            #axis.update({'units': {**_POSITIONERS_ROTATION, **_POSITIONERS_TRANSLATION}[axis.type]['units']},
+            #            force=True)
 
-    def write(self, command, *args, **kwargs):
-        return self.parent.write('{:d}{}'.format(self.num, command),
-                                 *args, **kwargs)
+            num += 1
 
-    @Feat()
-    def idn(self):
-        return self.query('ID?')
-
-    @Action()
-    def on(self):
-        """Put axis on"""
-        self.write('MO')
-
-    @Action()
-    def off(self):
-        """Put axis on"""
-        self.write('MF')
-
-    @Feat(values={True: '1', False: '0'})
-    def is_on(self):
-        """
-        :return: True is axis on, else false
-        """
-        return self.query('MO?')
-
-    @Action(units='mm')
-    def define_home(self, val=0):
-        """Remap current position to home (0), or to new position
-
-        :param val: new position"""
-        self.write('DH%f' % val)
-
-    @Action()
-    def home(self):
-        """Execute the HOME command"""
-        self.write('OR')
-
-    @Feat(units='mm')
+    # COPY FROM MotionControllerMultiAxis beacuse inheretence does not work
+    @Feat(read_once=False)
     def position(self):
-        return self.query('TP?')
+        return [axis.position for axis in self.axes]
+
+    @Feat(read_once=False)
+    def _position_cached(self):
+        return [axis.recall('position') for axis in self.axes]
 
     @position.setter
     def position(self, pos):
-        """
-        Waits until movement is done if self.wait_until_done = True.
-
-        :param pos: new position
-        """
-        if not self.is_on:
-            self.log_error('Axis not enabled. Not moving!')
-            return
-
-        # First do move to extra position if necessary
-        self._set_position(pos, wait=self.wait_until_done)
-
-
-    def __set_position(self, pos):
-        """
-        Move stage to a certain position
-        :param pos: New position
-        """
-        self.write('PA%f' % (pos))
-        self.last_set_position = pos
-
-    @Feat(units='mm/s')
-    def max_velocity(self):
-        return float(self.query('VU?'))
-
-    @max_velocity.setter
-    def max_velocity(self, velocity):
-        self.write('VU%f' % (velocity))
-
-    @Feat(units='mm/s**2')
-    def max_acceleration(self):
-        return float(self.query('AU?'))
-
-    @max_acceleration.setter
-    def max_acceleration(self, velocity):
-        self.write('AU%f' % (velocity))
-
-    @Feat(units='mm/s')
-    def velocity(self):
-        return float(self.query('VA?'))
-
-    @velocity.setter
-    def velocity(self, velocity):
-        """
-        :param velocity: Set the velocity that the axis should use when moving
-        :return:
-        """
-        self.write('VA%f' % (velocity))
-
-    @Feat(units='mm/s**2')
-    def acceleration(self):
-        return float(self.query('VA?'))
-
-    @acceleration.setter
-    def acceleration(self, acceleration):
-        """
-        :param acceleration: Set the acceleration that the axis should use
-                             when starting
-        :return:
-        """
-        self.write('AC%f' % (acceleration))
-
-    @Feat(units='mm/s')
-    def actual_velocity(self):
-        return float(self.query('TV'))
-
-    @actual_velocity.setter
-    def actual_velocity(self, val):
-        raise NotImplementedError
-
-    @Action()
-    def stop(self):
-        """Emergency stop"""
-        self.write('ST')
-
-    @Feat(values={True: '1', False: '0'})
-    def motion_done(self):
-        return self.query('MD?')
-
-    # Not working yet, see https://github.com/hgrecco/lantz/issues/35
-    # @Feat(values={Q_('encodercount'): 0,
-    #                     Q_('motor step'): 1,
-    #                     Q_('millimeter'): 2,
-    #                     Q_('micrometer'): 3,
-    #                     Q_('inches'): 4,
-    #                     Q_('milli-inches'): 5,
-    #                     Q_('micro-inches'): 6,
-    #                     Q_('degree'): 7,
-    #                     Q_('gradian'): 8,
-    #                     Q_('radian'): 9,
-    #                     Q_('milliradian'): 10,
-    #                     Q_('microradian'): 11})
-    @Feat()
-    def units(self):
-        ret = int(self.query(u'SN?'))
-        return UNITS[ret]
-
-    @units.setter
-    def units(self, val):
-        # No check implemented yet
-        self.write('%SN%' % (self.num, UNITS.index(val)))
-        super().units = val
-
-    def _wait_until_done(self):
-        # wait_time = convert_to('seconds', on_dimensionless='warn')(self.wait_time)
-        time.sleep(self.wait_time)
-        while not self.motion_done:
-            time.sleep(self.wait_time)
-        return True
+        """Move to position (x,y,...)"""
+        return self._position(pos)
--- lantz/drivers/smaract/tests/MCS2_test.py
+++ lantz/drivers/smaract/tests/MCS2_test.py
@@ -1,6 +1,9 @@
-from lantz.drivers.smaract.MCS2 import MCS2_Bare
+from lantz.drivers.smaract.MCS2 import MCS2_Minimum, MCS2_Axis, MCS2_Find_devices, MCS2
 import lantz
 import time
+import pint
+
+nm = lantz.Q_('nm')
 
 try:
     lantzlog
@@ -10,7 +13,37 @@ except NameError:
 
 #"usb:sn:MCS2-00000819"
 
+fd = MCS2_Find_devices()
+fd.initialize()
+devices = fd.find_devices()
+fd.finalize()
 
 #with MCS2_Bare('usb:ix:1') as smaract:
-with MCS2_Bare("usb:sn:MCS2-00000819") as smaract:
+with MCS2_Minimum("usb:sn:MCS2-00000819") as smaract:
     print(smaract.find_devices())
+    print(smaract.idn)
+    # Output is provided by log
+    smaract.read_position(1)
+    #smaract.move_channel(1, 10000, 0)
+    smaract.read_position(1)
+    axis = MCS2_Axis(parent=smaract, num=1, id='a', config={'accuracy': 10000  # pm
+                                                            })
+    #axis.units = 'pm'
+    axis.position
+    axis.move_mode
+    axis.states
+    #smaract.move_channel(1, 100000, 0)
+    #for i in range(10):
+    #    axis.states
+    #    time.sleep(0.01)
+    axis.position += 10*nm
+    #print(smaract.find_devices())
+
+    axis2 = MCS2_Axis(parent=smaract, num=3, id='rotation', config={'accuracy': 10000  # pm
+                                                           })
+    axis2.position
+    axis2.move_mode
+    axis2.states
+
+with MCS2("usb:sn:MCS2-00000819") as smaract:
+    smaract.position
