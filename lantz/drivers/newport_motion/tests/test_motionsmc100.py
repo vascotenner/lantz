970dbf00c47998123806596cc8121ca0b4810685
Vasco Tenner <v.t.tenner@vu.nl>
2019-05-02 16:21:13 +0200

    Newport motion test for smc100 is more explicit about needed connection information

--- lantz/drivers/newport_motion/tests/test_motionsmc100.py
+++ lantz/drivers/newport_motion/tests/test_motionsmc100.py
@@ -3,7 +3,7 @@ if __name__ == '__main__':
     import lantz.log
 
     parser = argparse.ArgumentParser(description='Test SMC100 driver')
-    parser.add_argument('-p', '--port', type=str, default='1',
+    parser.add_argument('-p', '--port', type=str, default='ASRL/dev/ttyUSB3::INSTR',
                         help='Serial port to connect to')
 
     args = parser.parse_args()
@@ -18,13 +18,11 @@ if __name__ == '__main__':
     lantz.messagebased._resource_manager = rm
 
     print(lantz.messagebased._resource_manager.list_resources())
-
+    print(args.port)
     with SMC100(args.port) as inst:
-    #with sm.via_serial(port=args.port) as inst:
-        inst.idn
         # inst.initialize() # Initialize the communication with the power meter
         # Find the status of all axes:
-        #for axis in inst.axes:
-        #    print('Axis {} Position {} is_on {} max_velocity {} velocity {}'.format(axis.num, axis.position,
-        #                                                                            axis.is_on, axis.max_velocity,
-        #                                                                            axis.velocity))
+        for axis in inst.axes:
+            print('Axis {} Position {} is_on {} max_velocity {} velocity {}'.format(axis.num, axis.position,
+                                                                                    axis.is_on, 0,#axis.max_velocity,
+                                                                                    axis.velocity))
